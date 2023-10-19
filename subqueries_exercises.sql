7704e9bc3663ae39f17569cde5d2ea0b9a74699c
Ryan Poole <ryan.anthony.poole@gmail.com>
2023-10-19 10:32:37 -0500

    Updated subqueries exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -2,11 +2,16 @@
 use employees;
 select concat(first_name,' ',last_name) Name, hire_date 
 	from employees
+    JOIN dept_emp as de
+    USING (EMP_NO)
     where hire_date = (select hire_date from employees where emp_no = '101010')
+    and de.to_date > now()
         ;
+	-- 55 rows returned
+    
 -- 2. Find all the titles ever held by all current employees with the first name Aamod.
 
-Select t.title, e.first_name, e.last_name
+Select distinct t.title
 	from titles as t
     join employees as e
     using (emp_no)
@@ -16,7 +21,8 @@ Select t.title, e.first_name, e.last_name
 	from employees 
 	where e.first_name = 'Aamod' and t.to_date > curdate())
     ;
-    
+-- 6 rows returned
+
 -- 3. How many people in the employees table are no longer working for the company?
  
 select count(*) 
@@ -34,19 +40,24 @@ from employees
 select emp_no
 	from dept_manager
     where to_date > curdate() and gender = 'f') ;
+-- 'Isamu Legleitner'
+-- 'Karsten Sigstam'
+-- 'Leon DasSarma'
+-- 'Hilary Kambil'
+
 
 -- 5. Find all the employees who currently have a higher salary than the companie's overall, 
 -- historical average salary.
 use employees;
-select concat(first_name,' ',last_name) as Name 
+select count(*) -- concat(first_name,' ',last_name) as Name 
 	from employees
     join salaries as s
     using (emp_no) 
     where s.to_date > curdate()
 		and s.salary > 
-		(select round(avg(salary),0)
-	from salaries)
-    group by name;
+		(select round(avg(salary),2)
+	from salaries);
+-- 154543
 
 select count(*) from salaries where to_date > curdate();
 -- 6. How many current salaries are within 1 standard 
@@ -80,6 +91,20 @@ limit 1;
 -- BONUS
 
 -- Find all the department names that currently have female managers.
+select dept_name
+	from departments as d
+    join dept_manager as dm
+    using (dept_no)
+    join employees as e
+    using (emp_no)
+    where e.last_name in( select concat(first_name,' ',last_name) as Managers
+from employees
+	where emp_no in (
+select emp_no
+	from dept_manager
+    where to_date > curdate() and gender = 'f'));
+    
+
 -- Find the first and last name of the employee with the highest salary.
 -- Find the department name that the employee with the highest salary works in.
 
