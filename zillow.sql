7e174f72c510ac6ad2cbed98c854c9ed12ec6ec6
Ryan Poole <ryan.anthony.poole@gmail.com>
2023-12-25 13:49:09 -0800

    edits

--- zillow.sql
+++ zillow.sql
@@ -1,13 +1,17 @@
 use zillow;
 select * from propertylandusetype;
+select fips, sum(fips) cnt from properties_2017
+group by fips;
+select fips, sum(fips) cnt from properties_2017
+group by fips;
 select
 	bedroomcnt, bathroomcnt,calculatedfinishedsquarefeet, taxvaluedollarcnt,
-	yearbuilt, taxamount, fips, storydesc
+	yearbuilt, taxamount, fips, numberofstories, garagecarcnt, poolcnt
     from properties_2017
     join propertylandusetype using (propertylandusetypeid)
-    join storytype using (storytypeid)
-    where propertylandusedesc in ('Single Family Residential')
-    group by storydesc;
+    where propertylandusedesc in ('Single Family Residential') 
+	and bedroomcnt > 0 and bathroomcnt > 0 and fips in ('6037')
+    ;
     
 select storydesc, count(storydesc) from storytype
 join properties_2017 using (storytypeid)
